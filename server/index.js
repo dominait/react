18bd4b97b56a2bd23ef0bb9e3bf113899b4e55cb
jimbits <contactjamesfoley@gmail.com>
2019-04-08 07:41:35 -0600

    react starter kit made from create react app

--- server/index.js
+++ server/index.js
@@ -1,28 +1,8 @@
 const express = require('express')
 const cors = require('cors')
-const boxrec = require('boxrec').default;
-let login = false;
  
-boxrec.login('Stingre', 'BUUDEL3Y2Y73')
-    .then(() => {
-        console.log('success');
-    }).then(()=> {
-        console.log('get data requests')
-        boxrec.getPersonById(80).then(boxer=>{
-            console.log(boxer.name)
-        })
-    })
-    .catch(error => {console.log(error) });
-    
-console.log(boxrec)
-
-
- 
-  
-
-
 const app = express()
-const port = 8000
+const port = 7000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
